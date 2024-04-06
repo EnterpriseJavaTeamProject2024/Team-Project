21a8ced19d8913403752c80778ea73f0867db948
student <ballard2@madisoncollege.edu>
2024-04-06 17:57:51 -0500

    added delete to FFRouter, not tested yet

--- src/main/java/funfactapi/main/FunFactRouter.java
+++ src/main/java/funfactapi/main/FunFactRouter.java
@@ -3,6 +3,7 @@ package funfactapi.main;
 import funfactapi.main.gets.GetFunFactByCategory;
 import funfactapi.main.gets.GetFunFactByID;
 import funfactapi.main.gets.GetRandomFunFact;
+import funfactapi.main.puts.DeleteFunFact;
 import funfactapi.main.puts.UpdateFunFact;
 
 import javax.ws.rs.ApplicationPath;
@@ -23,6 +24,7 @@ public class FunFactRouter extends Application {
         h.add(GetFunFactByID.class);
         h.add(GetFunFactByCategory.class);
         h.add(UpdateFunFact.class);
+        h.add(DeleteFunFact.class);
         return h;
     }
 }
